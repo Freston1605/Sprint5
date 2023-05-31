77f7b42f0f4faf3dd139ed509c2def825f6beba4
maofuentes <mfuentesoliva@comercioquinta.cl>
2023-05-30 20:16:40 -0400

    Crear Usuarios 20:16

--- sprint_5.sql
+++ sprint_5.sql
@@ -21,3 +21,38 @@ FLUSH PRIVILEGES;
 -- Usando la base de datos
 
 USE sprint_5;
+
+-- Creando la tabla de usuarios
+
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT,
+    nombre VARCHAR(50),
+    apellido VARCHAR(50),
+    edad INT,
+    correo VARCHAR(100),
+    veces_utilizado INT DEFAULT 1,
+    PRIMARY KEY (id)
+);
+
+-- Insertando 5 entradas a la tabla 'usuarios'
+
+INSERT INTO usuarios (nombre, apellido, edad, correo, veces_utilizado)
+VALUES
+    ('Juan', 'Pérez', 25, 'juan@correo.com', 3),
+    ('María', 'Gómez', 30, 'maria@correo.com', 2),
+    ('Pedro', 'López', 28, 'pedro@correo.com', 1),
+    ('Ana', 'Martínez', 35, 'ana@correo.com', 5),
+    ('Luis', 'Rodríguez', 32, 'luis@correo.com', 4);
+
+-- Creando la tabla de operadores
+
+CREATE TABLE operadores(
+	id INT AUTO_INCREMENT,
+	nombre VARCHAR(50),
+    apellido VARCHAR(50),
+    edad INT,
+    correo VARCHAR(100),
+    veces_utilizado INT DEFAULT 1,
+    PRIMARY KEY (id)
+
+);
